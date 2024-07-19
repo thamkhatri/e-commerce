10492e38e79a441612653b6c70df462f09b7658c
thumkhatri <thumkhatri2003@gmail.com>
2024-07-20 01:12:26 +0800

    new pages

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,51 @@
-import React from "react";
+// import React from "react";
 import "./Navbar.css";
 import logo from "../Assets/logo.png";
-// import cart_icon from "../Assets/cart_icon";
+import cart_icon from "../Assets/cart_icon.png";
+import React, { useState } from "react";
 
 const Navbar = () => {
+  const [menu, setMenu] = useState("shop");
   return (
     <div className="navbar">
       <div className="nav-logo">
         <img src={logo} alt="" />
       </div>
-      <ul className="nav-menu"></ul>
+      <ul className="nav-menu">
+        <li
+          onClick={() => {
+            setMenu("shop");
+          }}
+        >
+          SHOP{menu === "shop" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("makeup");
+          }}
+        >
+          MAKEUP {menu === "makeup" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("skincare");
+          }}
+        >
+          SKINCARE{menu === "skincare" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("bath&body");
+          }}
+        >
+          BATH&BODY {menu === "bath&body" ? <hr /> : <></>}
+        </li>
+      </ul>
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <img src={cart_icon} alt="" />
+        <div className="nav-cart-count">0</div>
+      </div>
     </div>
   );
 };
