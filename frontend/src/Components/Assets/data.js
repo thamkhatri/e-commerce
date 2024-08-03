06980659cb47aae916042a9ca08bbefd16f58880
thumkhatri <thumkhatri2003@gmail.com>
2024-08-04 03:45:38 +0800

    new collection

--- frontend/src/Components/Assets/data.js
+++ frontend/src/Components/Assets/data.js
@@ -1,41 +1,35 @@
-import p5_img from "./teatree.png";
-import p2_img from "./anessa.jpg";
-import p3_img from "./panteno.png";
-import p4_img from "./softymo.jpg";
-
-let data_product =[
+import p1_img from "./rom1.png.webp";
+import p2_img from "./rom2.jpg.webp";
+import p3_img from "./rom3.jpg.webp";
+import p4_img from "./rom5.jpg.webp";
+let data_product=[
     {
-        id:5,
-        name:"MEDIHEAL TEATREE NUDE GEL MASK 1PCS",
-        
-        image:p5_img,
-        new_price:4,
-        old_price:8,
+        id:1,
+        name:"#SPRING FEVER",
+        image:p1_img,
+        new_price:16.11,
+        old_price:17.90,
     },
     {
         id:2,
-        name:"SHISEIDO ANESSA PERFECT UV SUNSCREEN MILD MILK SPF 50+ PA++++ 60ML",
-       
+        name:"DEWFUL WATER TINT",
         image:p2_img,
-        new_price:38.56,
-        old_price:48.43,
+        new_price:16.56,
+        old_price:17.90,
     },
     {
         id:3,
-        name:"MEDIHEAL PANTENOLIPS SLEEPING LIP MASK",
-        
+        name:"[NEW] GLASTING GLOSS",
         image:p3_img,
-        new_price:9.46,
-        old_price:10.76,
+        new_price:16.46,
+        old_price:17.90,
     },
     {
         id:4,
-        name:"KOSE SOFTYMO WHITE CLEANSING OIL 240ML 2024 VERSION",
-       
+        name:"ROM& BLUSH",
         image:p4_img,
-        new_price:12.00,
-        old_price:15.35,
-    },
-    
-    ]
-    export default data_product;
+        new_price:15.00,
+        old_price:16.35,
+    }
+]
+export default data_product;
---
+++ frontend/src/Components/Assets/new_collections.js
@@ -0,0 +1,70 @@
+import p1_img from "./dat5.jpg.webp";
+import p2_img from "./dat2.jpg.webp";
+import p3_img from "./rom5.jpg.webp";
+import p4_img from "./dat3.jpg.webp";
+import p5_img from "./1.jpg.webp";
+import p6_img from "./juicy.jpg.webp";
+import p7_img from "./3.jpg.webp";
+import p8_img from "./dat4.jpg.webp";
+
+let new_collection =[
+    {
+        id:1,
+        name:"MOOD PEBBLE NUDE SERIES",
+        image:p1_img,
+        new_price:8.99,
+        old_price:12.89,
+    },
+    {
+        id:2,
+        name:"UNI LIQUID",
+        image:p2_img,
+        new_price:38.56,
+        old_price:48.43,
+    },
+    {
+        id:3,
+        name:"GLASTING GLOSS",
+        
+        image:p3_img,
+        new_price:19.46,
+        old_price:20.76,
+    },
+    {
+        id:4,
+        name:"BETTER SHAPE",
+        image:p4_img,
+        new_price:16.00,
+        old_price:17.90,
+    },
+    
+    {
+        id:5,
+        name:"SEE-THROUGH VIEL LIGHTER",
+        image:p5_img,
+        new_price:16.99,
+        old_price:17.89,
+    },
+    {
+        id:6,
+        name:"JUICY LASTING TINT ORIGINAL",
+        image:p6_img,
+        new_price:38.56,
+        old_price:48.43,
+    },
+    {
+        id:7,
+        name:"PALETTE ENERGY SERIES",
+        image:p7_img,
+        new_price:31.46,
+        old_price:34.76,
+    },
+    {
+        id:8,
+        name:"JUDYDOLL CONTOUR POW",
+        image:p8_img,
+        new_price:16.00,
+        old_price:17.90,
+    },
+    ]
+    export default new_collection;
