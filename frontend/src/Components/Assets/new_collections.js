b63f7614c1bff78802f0822353eb8d43103a198e
thumkhatri <thumkhatri2003@gmail.com>
2024-08-06 03:31:16 +0800

    offers

--- frontend/src/Components/Assets/new_collections.js
+++ frontend/src/Components/Assets/new_collections.js
@@ -2,10 +2,10 @@ import p1_img from "./dat5.jpg.webp";
 import p2_img from "./dat2.jpg.webp";
 import p3_img from "./rom5.jpg.webp";
 import p4_img from "./dat3.jpg.webp";
-import p5_img from "./1.jpg.webp";
-import p6_img from "./juicy.jpg.webp";
-import p7_img from "./3.jpg.webp";
-import p8_img from "./dat4.jpg.webp";
+// import p5_img from "./1.jpg.webp";
+// import p6_img from "./juicy.jpg.webp";
+// import p7_img from "./3.jpg.webp";
+// import p8_img from "./5.png.webp";
 
 let new_collection =[
     {
@@ -38,33 +38,33 @@ let new_collection =[
         old_price:17.90,
     },
     
-    {
-        id:5,
-        name:"SEE-THROUGH VIEL LIGHTER",
-        image:p5_img,
-        new_price:16.99,
-        old_price:17.89,
-    },
-    {
-        id:6,
-        name:"JUICY LASTING TINT ORIGINAL",
-        image:p6_img,
-        new_price:38.56,
-        old_price:48.43,
-    },
-    {
-        id:7,
-        name:"PALETTE ENERGY SERIES",
-        image:p7_img,
-        new_price:31.46,
-        old_price:34.76,
-    },
-    {
-        id:8,
-        name:"JUDYDOLL CONTOUR POW",
-        image:p8_img,
-        new_price:16.00,
-        old_price:17.90,
-    },
+    // {
+    //     id:5,
+    //     name:"SEE-THROUGH VIEL LIGHTER",
+    //     image:p5_img,
+    //     new_price:16.99,
+    //     old_price:17.89,
+    // },
+    // {
+    //     id:6,
+    //     name:"JUICY LASTING TINT ORIGINAL",
+    //     image:p6_img,
+    //     new_price:38.56,
+    //     old_price:48.43,
+    // },
+    // {
+    //     id:7,
+    //     name:"PALETTE ENERGY SERIES",
+    //     image:p7_img,
+    //     new_price:31.46,
+    //     old_price:34.76,
+    // },
+    // {
+    //     id:8,
+    //     name:"JUDYDOLL CONTOUR POW",
+    //     image:p8_img,
+    //     new_price:16.00,
+    //     old_price:17.90,
+    // },
     ]
     export default new_collection;
--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -3,7 +3,7 @@ import Hero from "../Components/Navbar/Hero";
 import Popular from "../Components/Popular/Popular";
 import Offers from "../Components/Offers/Offers";
 import NewCollections from "../Components/NewCollections/NewCollections";
-
+import NewsLetter from "../Components/NewsLetter/NewsLetter";
 const Shop = () => {
   return (
     <div>
@@ -11,6 +11,7 @@ const Shop = () => {
       <Popular />
       <Offers />
       <NewCollections />
+      {/* <NewsLetter /> */}
     </div>
   );
 };
