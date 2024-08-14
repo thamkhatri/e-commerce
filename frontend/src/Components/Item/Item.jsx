0864c3ee8b032ab7621fd19caea3603242cef47f
thumkhatri <thumkhatri2003@gmail.com>
2024-08-14 17:55:06 +0800

    last update

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./Item.css";
+import { Link } from "react-router-dom";
 
 const Item = (props) => {
+  console.log(props);
   return (
     <div className="item">
-      <img src={props.image} alt="" />
+      <Link to={`/product/${props.id}`}>
+        <img src={props.image} alt="" height={250} width={300} />
+      </Link>
       <p>{props.name}</p>
       <div className="item-prices">
         <div className="item-price-new">${props.new_price}</div>
--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+// import React, { useContext } from "react";
+// import { ShopContext } from "../Context/ShopContext";
+// import { useParams } from "react-router-dom";
+// import Breadcrum from "../Components/Breadcrums/Breadcrum";
+// import ProductDisplay from "../Components/ProductDisplay/ProductDisplay";
 
-const Product = () => {
-  return <div>this is product</div>;
-};
+// const Product = (props) => {
+//   const { all_product } = useContext(ShopContext);
+//   const { productId } = useParams();
+//   const product = all_product.find((e) => e.id === Number(productId));
 
-export default Product;
+//   return (
+//     <div>
+//       <Breadcrum product={product} />
+//       <ProductDisplay product={product} />
+//     </div>
+//   );
+// };
+
+// not wrokingggg destructure problem
+// export default Product;
